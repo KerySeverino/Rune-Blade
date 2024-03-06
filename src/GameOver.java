af67ff49aab4564455e983ce886026e1620283ac
AmadorA11 <161981590+AmadorA11@users.noreply.github.com>
2024-03-06 13:56:26 -0500

    GameOver class to display a game over screen (no mouse click function yet)

---
+++ src/GameOver.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+
+public class GameOver {
+	
+	//NOTE: Need to implement ability to click on the buttons
+
+	Image image = Toolkit.getDefaultToolkit().getImage("GameOver/game_over_PNG29.png");
+	
+	public void draw(Graphics g) 
+	{
+		//Background Outline
+		g.drawRect(300,25,750,600);
+		g.setColor(Color.RED);
+		g.fillRect(300,25,750,600);
+		
+		//Background Fill
+		g.drawRect(304,28,742,594);
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(304,28,742,594);
+		
+		//GAME OVER!
+		g.drawImage(image, 425, 60, 500, 200, null);
+		
+		//Button Outline
+		g.drawRect(350, 320, 650, 60);
+		g.drawRect(350, 450, 650, 60);
+		g.setColor(Color.YELLOW);
+		g.fillRect(350, 320, 650, 60);
+		g.fillRect(350, 450, 650, 60);
+		
+		//Button Fill
+		g.drawRect(352, 322, 646, 56);
+		g.drawRect(352, 452, 646, 56);
+		g.setColor(Color.BLUE);
+		g.fillRect(352, 322, 646, 56);
+		g.fillRect(352, 452, 646, 56);
+		
+		//Button Labels
+		Font font = new Font(null, Font.BOLD, 30);
+		g.setFont(font);
+		g.setColor(Color.WHITE);
+		g.drawString("TRY AGAIN", 600, 360);
+		g.drawString("QUIT", 640, 490);
+	
+		
+	}
+}
