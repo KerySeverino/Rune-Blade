32cb2bfd5b8f09913416e4d1321a9c28b91d7b03
Kery <Keryseverino@gmail.com>
2024-03-17 23:27:37 -0400

    Fix Camera and ImageLayer being empty

--- src/Camera.java
+++ src/Camera.java
@@ -1,4 +1,26 @@
-
 public class Camera {
+
+	static int x;
+	static int y;
+	
+	public static void moveLT(int dx)
+	{
+		x -= dx;
+	}
+	
+	public static void moveRT(int dx)
+	{
+		x += dx;
+	}
+	
+	public static void moveUp(int dy)
+	{
+		y -= dy;
+	}
+	
+	public static void moveDown(int dy)
+	{
+		y += dy;
+	}
 
 }
--- src/ImageLayer.java
+++ src/ImageLayer.java
@@ -1,4 +1,37 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
-public class ImageLayer {
+public class ImageLayer 
+{
 
+	public Image image;
+	
+	int x;
+	int y;
+	int z;
+	
+	public ImageLayer(String filename, int x, int y, int z) 
+	{
+		image = Toolkit.getDefaultToolkit().getImage(filename);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public void moveLT(int dx)
+	{
+		x += dx / z;
+	}
+	
+	public void moveRT(int dy) {
+		x -= dy / z;
+	}
+	
+	public void draw(Graphics pen)
+	{
+		for(int i = 0; i < 13; i++) {
+			pen.drawImage(image, x + i * 1800 - Camera.x / z, y - Camera.y / z, 1800, 1000, null);	
+		}
+	}
 }
